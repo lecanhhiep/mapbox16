b7e60a3dc0bfa1eb05fbac8fc357d1bdd095d3d3
jon-mly <jon.mly@gmail.com>
2025-06-17 12:03:41 +0200

    fix(1442): added dart:ui_web dependency in mapbox_gl_web

--- mapbox_gl_web/lib/mapbox_gl_web.dart
+++ mapbox_gl_web/lib/mapbox_gl_web.dart
@@ -9,23 +9,22 @@ import 'dart:html';
 import 'dart:js';
 import 'dart:js_util';
 import 'dart:math';
-import 'dart:typed_data';
 import 'dart:ui' as ui;
-import 'package:flutter/services.dart';
+import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' hide Element;
-
+import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:mapbox_gl_platform_interface/mapbox_gl_platform_interface.dart';
-import 'package:mapbox_gl_dart/mapbox_gl_dart.dart' hide Point, Source;
-import 'package:mapbox_gl_dart/mapbox_gl_dart.dart' as mapbox show Point;
 import 'package:image/image.dart' hide Point;
 import 'package:js/js_util.dart' as jsUtil;
+import 'package:mapbox_gl_dart/mapbox_gl_dart.dart' as mapbox show Point;
+import 'package:mapbox_gl_dart/mapbox_gl_dart.dart' hide Point, Source;
+import 'package:mapbox_gl_platform_interface/mapbox_gl_platform_interface.dart';
 import 'package:mapbox_gl_web/src/layer_tools.dart';
 
 part 'src/convert.dart';
 part 'src/mapbox_map_plugin.dart';
-part 'src/options_sink.dart';
 part 'src/mapbox_web_gl_platform.dart';
+part 'src/options_sink.dart';
